6b1528e67141626bf4565ddc9474f0cc500d75f4
animalen4 <anastacio202374@gmail.com>
2025-03-10 14:05:54 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,43 +1,37 @@
-// service-worker.js
+const CACHE_NAME = 'cool-cache';
 
-// Cuando el Service Worker se instala
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('v1').then((cache) => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/styles.css',
-                '/script.js',
-                '/manifest.json',
-                '/icon-192x192.png',
-                '/icon-512x512.png',
-            ]);
-        })
-    );
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = [
+    '/assets/',
+    '/src/'
+]
+
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        cache.addAll(PRECACHE_ASSETS);
+    })());
 });
 
-// Cuando se activa el Service Worker
-self.addEventListener('activate', (event) => {
-    // Eliminar cachés antiguos si es necesario
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cacheName) => {
-                    if (cacheName !== 'v1') {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
 });
 
-// Cuando se hace una solicitud, devuelve la respuesta desde la caché si está disponible
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(async () => {
+      const cache = await caches.open(CACHE_NAME);
+
+      // match the request to our cache
+      const cachedResponse = await cache.match(event.request);
+
+      // check if we got a valid response
+      if (cachedResponse !== undefined) {
+          // Cache hit, return the resource
+          return cachedResponse;
+      } else {
+        // Otherwise, go to the network
+          return fetch(event.request)
+      };
+  });
 });
